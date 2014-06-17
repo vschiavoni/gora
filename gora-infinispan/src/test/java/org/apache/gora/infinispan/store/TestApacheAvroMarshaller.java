4834937fd8b9f9c06fae322d04ecd70dcd696688
Valerio Schiavoni <valerio.schiavoni@gmail.com>
2014-06-17 18:04:09 +0200

    show the correct marshalling error problem due to the non serializable type of the Employee class

--- gora-infinispan/src/test/java/org/apache/gora/infinispan/store/TestApacheAvroMarshaller.java
+++ gora-infinispan/src/test/java/org/apache/gora/infinispan/store/TestApacheAvroMarshaller.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 import org.apache.gora.examples.generated.Employee;
 import org.infinispan.client.hotrod.marshall.ApacheAvroMarshaller;
+import org.infinispan.commons.CacheException;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,20 +14,28 @@ public class TestApacheAvroMarshaller {
 
 	private ApacheAvroMarshaller marsh;
 	private Employee e1;
+	
 	@Before
 	public void setUp() throws Exception {
 		marsh = new ApacheAvroMarshaller();
-		e1= new Employee();
-	}
-	
-	@Test
-	public void testMarshallEmployee() throws IOException, InterruptedException {
-		assertNotNull(marsh.objectToBuffer(e1));
+		this.e1  = new Employee();
 	}
 	
 	@Test
 	public void testMarshallString() throws IOException, InterruptedException {
-		assertNotNull(marsh.objectToByteBuffer("hello", 5));
+		assertNotNull(marsh.objectToByteBuffer("hello"));
+	}
+
+	@Test
+	public void testMarshallEmployee() throws IOException, InterruptedException {
+		try{
+			assertNotNull(marsh.objectToByteBuffer(e1)) ;
+		}
+		catch(CacheException ce){
+			String errorMessage = ce.getLocalizedMessage();
+			System.out.println(errorMessage);
+			assertEquals(errorMessage, "Unsupported type: class org.apache.gora.examples.generated.Employee");
+		}
 	}
 
 }
